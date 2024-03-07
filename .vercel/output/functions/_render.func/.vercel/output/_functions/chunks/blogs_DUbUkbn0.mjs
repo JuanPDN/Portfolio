ed6c767a6c7956486c2acd040748afdb9c4a96c7
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-03-07 10:12:07 -0500

    deploy

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/blogs_DUbUkbn0.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/blogs_CosvH6tc.mjs').then(n => n.b);
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/cv_P9nE6cMh.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/cv_DNphQ3h7.mjs');
-
-export { page };
