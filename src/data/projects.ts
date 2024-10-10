87bbeb5fb10e095aa8fcc6c4d6a9e23f7ed02a77
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-10-10 14:34:08 -0500

    feat: add translate of all proyects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,27 +2,27 @@ import type { Projects } from "./interfaces";
 
 const projects: Projects[] = [
     {
-        category: "last Projects",
+        category: "Últimos Proyectos",
         list: [
             {
-                name: "Simple image uploader",
-                summary: "A full-stack image upload application that allows users to upload images in a simple and intuitive way, Upload images in JPG, PNG, or GIF format with a maximum size of 2MB, Drag and drop file upload functionality, Share button to copy image address, Download button to save image, Browse and select files for upload.",
+                name: "Cargador de imágenes simple",
+                summary: "Una aplicación full-stack para subir imágenes que permite a los usuarios cargar imágenes de manera simple e intuitiva. Permite subir imágenes en formato JPG, PNG o GIF con un tamaño máximo de 2MB, funcionalidad de arrastrar y soltar para subir archivos, botón para compartir la dirección de la imagen, botón de descarga para guardar la imagen y opción de examinar y seleccionar archivos para cargar.",
                 image: "imageUpload.webp",
                 stack: ["NextJS", "Tailwind", "MySQL", "Docker", "NestJS", "Typescript", "React"],
                 ref: "https://simple-image-uploader-jp.vercel.app/",
                 repo: "https://github.com/JuanPDN/simple-image-uploader"
             },
             {
-                name: "Code Sharing App - NoteCode",
-                summary: "Application for sharing code snippets that allows users to store and share code with a generated ID. Include the option to choose language and theme, and disable the share button until edits are made. Responsive design for all devices.",
+                name: "Aplicación para compartir código - NoteCode",
+                summary: "Aplicación para compartir fragmentos de código que permite a los usuarios almacenar y compartir código con un ID generado. Incluye la opción de elegir el lenguaje y tema, y deshabilitar el botón de compartir hasta que se realicen ediciones. Diseño responsive para todos los dispositivos.",
                 image: "codeSharing.webp",
                 stack: ["VueJS", "MongoDB", "Typescript", "Docker", "ExpressJS", "NodeJS"],
                 ref: "https://code-sharing-app-jp.vercel.app/",
                 repo: "https://github.com/JuanPDN/code-sharing-app"
             },
             {
-                name: "Country Page",
-                summary: "A country ranking page displaying data such as population, area, and name in a table. It allows sorting, filtering by regions and status (UN members, independent), and searching by name. It provides details of each country and its neighbors on individual pages. Responsive design and pagination option.",
+                name: "Página de países",
+                summary: "Una página de clasificación de países que muestra datos como población, área y nombre en una tabla. Permite ordenar, filtrar por regiones y estado (miembros de la ONU, independientes) y buscar por nombre. Proporciona detalles de cada país y sus vecinos en páginas individuales. Diseño responsive y opción de paginación.",
                 image: "countryPage.webp",
                 stack: ["NextJS", "Tailwind", "Typescript", "React"],
                 ref: "https://country-page-jp.vercel.app",
@@ -31,67 +31,67 @@ const projects: Projects[] = [
         ]
     },
     {
-        category: "web projects",
+        category: "Proyectos web",
         list: [
             {
-                name: "Country Page",
-                summary: "A country ranking page displaying data such as population, area, and name in a table. It allows sorting, filtering by regions and status (UN members, independent), and searching by name. It provides details of each country and its neighbors on individual pages. Responsive design and pagination option.",
+                name: "Página de países",
+                summary: "Una página de clasificación de países que muestra datos como población, área y nombre en una tabla. Permite ordenar, filtrar por regiones y estado (miembros de la ONU, independientes) y buscar por nombre. Proporciona detalles de cada país y sus vecinos en páginas individuales. Diseño responsive y opción de paginación.",
                 image: "countryPage.webp",
                 stack: ["NextJS", "Tailwind", "Typescript", "React"],
                 ref: "https://country-page-jp.vercel.app",
                 repo: "https://github.com/JuanPDN/country-page"
             },
             {
-                name: "Random Quote",
-                summary: "Application to generate random quotes using HTML, CSS and JavaScript. It includes the display of random quotes, a button to generate new quotes, and the option to copy quotes to the clipboard. Responsive design for different screen sizes.",
+                name: "Cita aleatoria",
+                summary: "Aplicación para generar citas aleatorias utilizando HTML, CSS y JavaScript. Incluye la visualización de citas aleatorias, un botón para generar nuevas citas y la opción de copiar citas al portapapeles. Diseño responsive para diferentes tamaños de pantalla.",
                 image: "randomQuote.webp",
                 stack: ["JavaScript", "CSS", "HTML"],
                 ref: "https://random-quote-jp.vercel.app/",
                 repo: "https://github.com/JuanPDN/random-quote"
             },
             {
-                name: "GitHub Profile",
-                summary: "GitHub profile search application using HTML, CSS and JavaScript. It allows you to search by username, view followers, followers, location and repositories, and access details of each repository. Responsive and functional design.",
+                name: "Perfil de GitHub",
+                summary: "Aplicación de búsqueda de perfiles de GitHub utilizando HTML, CSS y JavaScript. Permite buscar por nombre de usuario, ver seguidores, ubicaciones y repositorios, y acceder a detalles de cada repositorio. Diseño funcional y responsive.",
                 image: "githubProfile.webp",
                 stack: ["Typescript", "Tailwind", "React", "NextJS"],
                 ref: "https://git-hub-profile-jp.vercel.app/",
                 repo: "https://github.com/JuanPDN/GitHub-profile"
             },
             {
-                name: "Contact Page",
-                summary: "Development of a contact page that uses HTML and CSS to create a functional form and responsive design. It includes icons, input fields with placeholders, a dropdown with options, and a button, all adapted for different screen sizes.",
+                name: "Página de contacto",
+                summary: "Desarrollo de una página de contacto que utiliza HTML y CSS para crear un formulario funcional y un diseño responsive. Incluye íconos, campos de entrada con marcadores de posición, un menú desplegable con opciones y un botón, todo adaptado para diferentes tamaños de pantalla.",
                 image: "contactPage.webp",
                 stack: ["JavaScript", "CSS", "HTML", "Tailwind"],
                 ref: "https://contact-page-jp.vercel.app/",
                 repo: "https://github.com/JuanPDN/contact-page"
             },
             {
-                name: "Simple Coffee Listing",
-                summary: "Coffee listing page using Svelte. Includes a reusable card component with image, name, price, rating and number of votes. Displays labels and availability statuses conditionally and allows you to list all products or only those available. Uses data from an API or JS file.",
+                name: "Listado simple de café",
+                summary: "Página de listado de café utilizando Svelte. Incluye un componente de tarjeta reutilizable con imagen, nombre, precio, calificación y número de votos. Muestra etiquetas y estados de disponibilidad de manera condicional, y permite listar todos los productos o solo los disponibles. Utiliza datos de una API o archivo JS.",
                 image: "listCoffee.webp",
                 stack: ["Svelte", "CSS", "Typescript"],
                 ref: "https://simple-coffee-listing-jp.vercel.app/",
                 repo: "https://github.com/JuanPDN/Simple-Coffee-Listing"
             },
             {
-                name: "Simple Homepage",
-                summary: "Home page using HTML, CSS and JavaScript. It includes a logo, header, description, links and buttons, with functionalities to switch between light and dark mode and lateral navigation on mobiles. Responsive design for different screen sizes.",
+                name: "Página de inicio simple",
+                summary: "Página de inicio utilizando HTML, CSS y JavaScript. Incluye un logotipo, encabezado, descripción, enlaces y botones, con funcionalidades para cambiar entre modo claro y oscuro y navegación lateral en móviles. Diseño responsive para diferentes tamaños de pantalla.",
                 image: "homePage.webp",
                 stack: ["JavaScript", "CSS", "HTML", "Tailwind"],
                 ref: "https://home-page-dev.vercel.app/",
                 repo: "https://github.com/JuanPDN/home-page"
             },
             {
-                name: "Testimonial Page",
-                summary: "Creation of a testimonial page using advanced HTML and CSS, including grid, media queries and icons. The project includes two testimonial cards and a responsive design for different screen sizes.",
+                name: "Página de testimonios",
+                summary: "Creación de una página de testimonios utilizando HTML y CSS avanzados, incluyendo grid, media queries e íconos. El proyecto incluye dos tarjetas de testimonios y un diseño responsive para diferentes tamaños de pantalla.",
                 image: "testimonialPage.webp",
                 stack: ["JavaScript", "Tailwind", "HTML"],
                 ref: "https://testimonial-page-jp.vercel.app/",
                 repo: "https://github.com/JuanPDN/testimonial-page"
             },
             {
-                name: "Blog de Cafe",
-                summary: "A comprehensive coffee blog offering articles, reviews, brewing techniques, discussions about coffee culture, and courses for enthusiasts to deepen their knowledge and skills in the world of coffee.",
+                name: "Blog de Café",
+                summary: "Un completo blog de café que ofrece artículos, reseñas, técnicas de preparación, discusiones sobre la cultura del café y cursos para que los entusiastas profundicen su conocimiento y habilidades en el mundo del café.",
                 image: "blog-cafe.webp",
                 stack: ["JavaScript", "CSS", "HTML"],
                 ref: "https://3rdsite.netlify.app",
@@ -99,108 +99,60 @@ const projects: Projects[] = [
         ]
     },
     {
-        category: "mobile projects",
+        category: "Proyectos móviles",
         list: [
-
             {
                 name: "TokTik",
-                summary: "The TikTokClone application is a visually stunning social media platform designed to replicate the engaging and entertaining user experience of the popular TikTok app.",
+                summary: "La aplicación TikTokClone es una plataforma de redes sociales visualmente impresionante, diseñada para replicar la experiencia de usuario atractiva y entretenida de la popular aplicación TikTok.",
                 image: "totik.webp",
                 stack: ["Flutter", "Dart"],
                 repo: "https://github.com/JuanPDN/TokTik",
             },
-
             {
-                name: "yes no app",
-                summary: "The AutoResponse Chat App streamlines communication by automatically generating predefined messages for yes and no responses. Users can customize messages and set up triggers easily. Available on web, mobile, and desktop platforms, it ensures prompt and efficient replies across various channels.",
+                name: "Aplicación de respuestas automáticas",
+                summary: "La AutoResponse Chat App simplifica la comunicación generando automáticamente mensajes predefinidos para respuestas de sí y no. Los usuarios pueden personalizar los mensajes y configurar activadores fácilmente. Disponible en plataformas web, móviles y de escritorio, garantiza respuestas rápidas y eficientes en diversos canales.",
                 image: "yes-no-app.webp",
                 stack: ["Flutter", "Dart"],
                 repo: "https://github.com/JuanPDN/yes_no_app",
             },
             {
-                name: "cinemapedia",
-                summary: "The Upcoming Movies App keeps users informed about the latest releases and upcoming films in theaters. With a user-friendly interface, it provides details such as release dates, trailers, cast information, and reviews. Users can explore upcoming movies, bookmark favorites, and receive notifications for premiere dates.",
+                name: "Cinemapedia",
+                summary: "La aplicación de películas próximas mantiene a los usuarios informados sobre los últimos estrenos y las películas próximas en cines. Con una interfaz fácil de usar, proporciona detalles como fechas de estreno, tráilers, información del reparto y reseñas. Los usuarios pueden explorar películas próximas, marcar favoritos y recibir notificaciones para fechas de estreno.",
                 image: "cinemapedia.webp",
                 stack: ["Flutter", "Dart"],
                 repo: "https://github.com/JuanPDN/cinemapedia",
             },
             {
-                name: "app shop",
-                summary: "The Product Management App is a user-friendly tool tailored for businesses to efficiently oversee their product inventory. With a focus on simplicity and effectiveness, this app simplifies the process of adding, editing, and organizing products. It enables users to maintain accurate product information, streamline inventory management, and make informed decisions to optimize product offerings.",
+                name: "Aplicación de tienda",
+                summary: "La aplicación de gestión de productos es una herramienta fácil de usar, diseñada para que las empresas supervisen su inventario de productos de manera eficiente. Con un enfoque en la simplicidad y efectividad, esta app simplifica el proceso de agregar, editar y organizar productos. Permite a los usuarios mantener información precisa de los productos, optimizar la gestión del inventario y tomar decisiones informadas.",
                 image: "shop app.webp",
                 stack: ["Flutter", "Dart"],
                 repo: "https://github.com/JuanPDN/app_shop",
             }
-
         ]
     },
     {
-        category: "web applications",
+        category: "Aplicaciones web",
         list: [
             {
-                name: "Simple image uploader",
-                summary: "A full-stack image upload application that allows users to upload images in a simple and intuitive way, Upload images in JPG, PNG, or GIF format with a maximum size of 2MB, Drag and drop file upload functionality, Share button to copy image address, Download button to save image, Browse and select files for upload.",
+                name: "Cargador de imágenes simple",
+                summary: "Una aplicación full-stack para subir imágenes que permite a los usuarios cargar imágenes de manera simple e intuitiva. Permite subir imágenes en formato JPG, PNG o GIF con un tamaño máximo de 2MB, funcionalidad de arrastrar y soltar para subir archivos, botón para compartir la dirección de la imagen, botón de descarga para guardar la imagen y opción de examinar y seleccionar archivos para cargar.",
                 image: "imageUpload.webp",
                 stack: ["NextJS", "Tailwind", "MySQL", "Docker", "NestJS", "Typescript", "React"],
                 ref: "https://simple-image-uploader-jp.vercel.app/",
                 repo: "https://github.com/JuanPDN/simple-image-uploader"
             },
             {
-                name: "Code Sharing App - NoteCode",
-                summary: "Application for sharing code snippets that allows users to store and share code with a generated ID. Include the option to choose language and theme, and disable the share button until edits are made. Responsive design for all devices.",
+                name: "Aplicación para compartir código - NoteCode",
+                summary: "Aplicación para compartir fragmentos de código que permite a los usuarios almacenar y compartir código con un ID generado. Incluye la opción de elegir el lenguaje y tema, y deshabilitar el botón de compartir hasta que se realicen ediciones. Diseño responsive para todos los dispositivos.",
                 image: "codeSharing.webp",
                 stack: ["VueJS", "MongoDB", "Typescript", "Docker", "ExpressJS", "NodeJS"],
                 ref: "https://code-sharing-app-jp.vercel.app/",
                 repo: "https://github.com/JuanPDN/code-sharing-app"
-            },
-            {
-                name: "QR Code Generator",
-                summary: "Generate QR codes with HTML, CSS and JavaScript. It allows you to enter a URL, generate a QR code, download the image and copy the code to the clipboard. Responsive design for different screen sizes.",
-                image: "qrCode.webp",
-                stack: ["JavaScript", "CSS", "HTML", "Tailwind"],
-                ref: "https://qr-code-generator-jp.vercel.app",
-                repo: "https://github.com/JuanPDN/qr-code-generator"
-            },
-            {
-                name: "Music Player",
-                summary: "Music player with HTML, CSS and JavaScript. Play, stop, fast forward and rewind songs, and adjust playback with a progress bar. Responsive design.",
-                image: "musicPlayer.webp",
-                stack: ["JavaScript", "CSS", "HTML"],
-                ref: "https://music-player-jp.vercel.app/",
-                repo: "https://github.com/JuanPDN/music-player"
-            },
-            {
-                name: "Translate App",
-                summary: "Translation application using an API, with support to translate texts of up to 500 characters. It includes options to translate between multiple languages, see updates in real time, listen to translated texts and copy the text. Interactive and functional design.",
-                image: "translateApp.webp",
-                stack: ["Typescript", "React", "Tailwind"],
-                ref: "https://translate-app-jp.vercel.app/",
-                repo: "https://github.com/JuanPDN/translate-app"
-            }
-
-        ]
-    },
-    {
-        category: "web games",
-        list: [
-            {
-                name: "Guess The Word Game",
-                summary: "Game in which users guess a messy word. Using HTML, CSS and JavaScript. It includes letter input, error tracking, and buttons to regenerate words and restart the game. Responsive design for various screen sizes.",
-                image: "guessWord.webp",
-                stack: ["JavaScript", "CSS", "HTML", "Tailwind"],
-                ref: "https://juanpdn.github.io/Guess-the-word/",
-                repo: "https://github.com/JuanPDN/Guess-the-word"
-            },
-            {
-                name: "Country Quiz",
-                summary: "Application of questionnaire on countries using an API to generate 10 questions with 4 options each. It shows the correct answer immediately, allows you to navigate between questions, and presents a page of results and congratulations when completing the questionnaire. Interactive and functional design.",
-                image: "countryQuiz.webp",
-                stack: ["Typescript", "Angular", "HTML", "CSS"],
-                ref: "https://country-quiz-jp.vercel.app/",
-                repo: "https://github.com/JuanPDN/country-quiz"
             }
         ]
     }
-]
+];
+
 
 export default projects
